41be63b223f3b45fe25a39f70bb8daf75e8c1671
♠ Sk1M4x ♠ <5k1m4x@gmail.com>
2015-02-21 23:49:38 -0200

    Create Check_Modules.py

---
+++ Check_Modules.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# InfoIP Project
+
+# Official Repository:
+# https://github.com/sk1m4x/InfoIP/
+
+try:
+	import sys
+except ImportError:
+	print "sys is not installed."
+
+try:
+	import json
+except ImportError:
+	print "json is not installed."
+
+try:
+	import requests
+except ImportError:
+	print "requests is not installed."
+
+try:
+	import socket
+except ImportError:
+	print "socket is not installed."
+
+try:
+	import os
+except ImportError:
+	print "os is not installed."
+
+try:
+	import time
+except ImportError:
+	print "time is not installed."
